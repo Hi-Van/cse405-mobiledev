775d96f1d86bab1f044d398835d415096a1e3138
Vanouphon Sirisouk <sirisoukvanouphon@gmail.com>
2022-04-11 00:33:03 -0400

    lab 3 update

--- a2022_02_07-Rainbow2.05/app/src/main/java/com/example/a2022_02_07/MainActivity.kt
+++ a2022_02_07-Rainbow2.05/app/src/main/java/com/example/a2022_02_07/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 
@@ -18,24 +20,50 @@ const val DEFAULT_COLOR = "#FFFFFF" // White
 
 class MainActivity : AppCompatActivity() {
 
+    //function fixed using the folowing documentation https://developer.android.com/training/basics/intents/result
+
+    private val startForResult= registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { ActivityPromise: ActivityResult  ->
+
+        val promiseState = ActivityPromise.resultCode
+
+        if (promiseState == Activity.RESULT_OK) {
+
+            val data = ActivityPromise.data
+            val backgroundColor = data?.getIntExtra(RAINBOW_COLOR, Color.parseColor(DEFAULT_COLOR)) ?: Color.parseColor(DEFAULT_COLOR)
+            val colorName = data?.getStringExtra(RAINBOW_COLOR_NAME) ?: ""
+            val colorMessage = getString(R.string.color_chosen_message, colorName)
+
+            val rainbowColor = findViewById<TextView>(R.id.rainbow_color)
+
+            rainbowColor.setBackgroundColor(ContextCompat.getColor(this, backgroundColor))
+            rainbowColor.text = colorMessage
+            rainbowColor.isVisible = true
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.submit_button).setOnClickListener {
             //Set the name of the Activity to launch passing in request code
-            Intent(this, RainbowColorPickerActivity::class.java)
+
+            startForResult.launch(Intent(this, RainbowColorPickerActivity::class.java))
+
+            //old code
+            /*Intent(this, RainbowColorPickerActivity::class.java)
                 .also { rainbowColorPickerIntent ->
                     startActivityForResult(
                         rainbowColorPickerIntent,
                         PICK_RAINBOW_COLOR_INTENT
                     )
-                }
+                }*/
+
         }
 
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+    /*override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
         if (requestCode == PICK_RAINBOW_COLOR_INTENT && resultCode == Activity.RESULT_OK) {
@@ -50,6 +78,5 @@ class MainActivity : AppCompatActivity() {
             rainbowColor.text = colorMessage
             rainbowColor.isVisible = true
         }
-    }
-
+    }*/
 }
