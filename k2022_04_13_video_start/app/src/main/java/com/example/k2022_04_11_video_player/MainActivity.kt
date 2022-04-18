6217c13205adf9bac2ce88047df1c2b6d4500e48
Hi-Van <sirisoukvanouphon@gmail.com>
2022-04-18 13:42:53 -0400

    lab 6 update

--- k2022_04_13_video_start/app/src/main/java/com/example/k2022_04_11_video_player/MainActivity.kt
+++ k2022_04_13_video_start/app/src/main/java/com/example/k2022_04_11_video_player/MainActivity.kt
@@ -6,11 +6,9 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.SurfaceHolder
+import android.view.View
 import android.view.WindowManager
-import android.widget.Button
-import android.widget.MediaController
-import android.widget.SeekBar
-import android.widget.VideoView
+import android.widget.*
 import androidx.appcompat.app.ActionBarDrawerToggle
 import java.net.URI
 
@@ -24,7 +22,8 @@ class MainActivity : AppCompatActivity(), MediaPlayer.OnPreparedListener, MediaP
     var videoOn: Boolean = false
     var firstTimeOn: Boolean = true
 
-    val videoStr = "https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1649895850/ei/ShVXYt3qDom58wSPuKnoAQ/ip/137.99.236.44/id/DDU-rZs-Ic4.3/itag/96/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D137/hls_chunk_host/rr2---sn-jvooxqouf3-cqaz.googlevideo.com/playlist_duration/30/manifest_duration/30/spc/4ocVC1ZALQguP-a74YvsBC9DzK_D/vprv/1/playlist_type/DVR/initcwndbps/8200/mh/PU/mm/44/mn/sn-jvooxqouf3-cqaz/ms/lva/mv/m/mvi/2/pl/16/dover/11/pacing/0/keepalive/yes/fexp/24001373,24007246/mt/1649873800/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,playlist_duration,manifest_duration,spc,vprv,playlist_type/sig/AOq0QJ8wRQIgPgxHeOEKFSjbPV5pCwM1iCRyO5sF9WvD-qwV5-zQXRkCIQDIjiBZPdpc7xnp98zjpD5Cz7unJtnO6MV9K_c_IThZGg%3D%3D/lsparams/hls_chunk_host,initcwndbps,mh,mm,mn,ms,mv,mvi,pl/lsig/AG3C_xAwRQIgc5M7PfIdVtJ9Phe_YuvYSBTTWuFzHt2kG2y5a6ocNhUCIQDwPZbCYRqv1T7Uqva2NolZjhDfXLEgbEh5ld7zFuXRdg%3D%3D/playlist/index.m3u8"
+    val videoStr = "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ElephantsDream.mp4"
+    //"https://manifest.googlevideo.com/api/manifest/hls_playlist/expire/1649895850/ei/ShVXYt3qDom58wSPuKnoAQ/ip/137.99.236.44/id/DDU-rZs-Ic4.3/itag/96/source/yt_live_broadcast/requiressl/yes/ratebypass/yes/live/1/sgoap/gir%3Dyes%3Bitag%3D140/sgovp/gir%3Dyes%3Bitag%3D137/hls_chunk_host/rr2---sn-jvooxqouf3-cqaz.googlevideo.com/playlist_duration/30/manifest_duration/30/spc/4ocVC1ZALQguP-a74YvsBC9DzK_D/vprv/1/playlist_type/DVR/initcwndbps/8200/mh/PU/mm/44/mn/sn-jvooxqouf3-cqaz/ms/lva/mv/m/mvi/2/pl/16/dover/11/pacing/0/keepalive/yes/fexp/24001373,24007246/mt/1649873800/sparams/expire,ei,ip,id,itag,source,requiressl,ratebypass,live,sgoap,sgovp,playlist_duration,manifest_duration,spc,vprv,playlist_type/sig/AOq0QJ8wRQIgPgxHeOEKFSjbPV5pCwM1iCRyO5sF9WvD-qwV5-zQXRkCIQDIjiBZPdpc7xnp98zjpD5Cz7unJtnO6MV9K_c_IThZGg%3D%3D/lsparams/hls_chunk_host,initcwndbps,mh,mm,mn,ms,mv,mvi,pl/lsig/AG3C_xAwRQIgc5M7PfIdVtJ9Phe_YuvYSBTTWuFzHt2kG2y5a6ocNhUCIQDwPZbCYRqv1T7Uqva2NolZjhDfXLEgbEh5ld7zFuXRdg%3D%3D/playlist/index.m3u8"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,15 +32,34 @@ class MainActivity : AppCompatActivity(), MediaPlayer.OnPreparedListener, MediaP
 
         videoView = findViewById(R.id.videoView)
         videoToggle = findViewById(R.id.video_toggle_button)
+        mediaPlayer = MediaPlayer()
+        //mediaController = findViewById(R.id.media_controller)
 
         mediaController = MediaController(this)
         mediaController.setAnchorView(videoView)
+        //mediaController.setMediaPlayer(mediaPlayer)
+        //mediaController.isEnabled = true
         videoView.setMediaController(mediaController)
-
         videoView.holder.addCallback(this)
 
-        mediaPlayer = MediaPlayer()
+        mediaController.setPrevNextListeners({
+           object: View.OnClickListener {
+               override fun onClick(p0: View?) {
+                   Toast.makeText(baseContext,"Previous", Toast.LENGTH_SHORT).show()
+               }
+           }
+
+        }, {
+            object: View.OnClickListener {
+                override fun onClick(p0: View?) {
+                    Toast.makeText(baseContext,"Next", Toast.LENGTH_SHORT).show()
+                }
+            }
+        });
+
         mediaPlayer.setOnPreparedListener(this)
+
+        //mediaController.show()
 
         videoToggle.setOnClickListener {
             if (videoOn) {
@@ -63,6 +81,8 @@ class MainActivity : AppCompatActivity(), MediaPlayer.OnPreparedListener, MediaP
 
     override fun onPrepared(mediaPlayer: MediaPlayer) {
         mediaPlayer.start()
+        mediaController.requestFocus()
+        //mediaController.show()
     }
 
     override fun onDrmInfo(mediaPlayer: MediaPlayer, drmInfo: MediaPlayer.DrmInfo?) {
